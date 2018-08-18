5895b38423274980b9a0aab55e0213188f8c5bdf
cecy-rodriguez <42384269+cecy-rodriguez@users.noreply.github.com>
2018-08-17 23:36:48 -0500

    Initial commit

---
+++ src/ex03_1_exercise/ShoppingCart.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex03_1_exercise;
+
+/**
+ *
+ * @author Erika
+ */
+public class ShoppingCart {
+    public static void main (String[] args){
+        System.out.println("< your message here >");
+     }
+}
